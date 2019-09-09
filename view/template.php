1f14ba73de633cc3fa425eb906b488e705a1fc64
Aline <alinesaoseba15@gmail.com>
2019-09-09 10:12:58 -0300

    l

--- view/template.php
+++ view/template.php
@@ -8,8 +8,70 @@
         <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css" type="text/css">
     </head>
     <body>
+        <nav class="navbar topnav">
+            <div class="container">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="<?php echo BASE_URL; ?>">Home</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>contact">Contact</a></li>
+                </ul>
+                <ul class="nav navbar-nav nav-right">
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">English<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">English</a></li>
+                            <li><a href="#">Portuguese</a>
+                            </li>
+                            <li><a href="Espanish"></a></li>
+                        </ul>
+                    </li>
+                    <li><a href="<?php echo BASE_URL;?>login">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+        
+        <!--
+        Contato/Carrinho
+        -->
+        
         <header>
-
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-2 logo">
+                        <a href="<?php echo BASE_URL;?>"><img src="<?php echo BASE_URL;?>asserts/images/logo.png"/></a>
+                    </div>
+                    <div class="col-sm-7">
+                        <div class="head_help">(12)981236548</div>
+                        <div class="head_email">
+                            
+                            contato@<span>deltali.com.br</span>
+                        </div>
+                        <div class="serch_area">
+                            <form method="GET">
+                                <input type="text" name="s" required placeholder="Search item"/>
+                                <select name="category">
+                                    <option value="">All categories</option>
+                                </select>
+                                <input type="submit" value=""/>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <a href="<?php echo BASE_URL;?>cart">
+                            <div class="cartarea">
+                                <div class="carticon">
+                                    <div class="cartqt">
+                                        9
+                                    </div>
+                                </div>
+                                <div class="cattotal">
+                                    Your cart:<br/>
+                                    <span>R$ 999,99</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>        
         </header>
         <main>
 
